d0de7ee24c4fd772e0f60762e2ef86aa7546ba66
Joris Placette <joris@placette.fr>
2019-04-02 12:37:31 +0200

    debut t8

--- TACHE7/performance_simplification.sh
+++ TACHE7/performance_simplification.sh
@@ -1,13 +1,19 @@
-if [ $# -eq 3 ]
+time if [ $# -eq 3 ]
 then
 
   for image in $1*
   do
-    #echo "execution sur $image , sorties sur $2/$image.eps"  
-    time ./t7_simplif_douglas_bezier $image $2/$(basename $image) $3
-  done
+    for mode in s b1 b2
+    do
+        for precision in 0 0.5 1 2 4 8 16
+        do
+            #echo "execution sur $image , sorties sur $2/$image.eps"  
+            ./t7_simplif_douglas_bezier $image $2/$(basename $image)_$mode_$precision $3 $4
+        done
+    done
+done
 
 else 
 
-  echo "$0 <repertoire images> <repertoire de sortie>"
+  echo "$0 <repertoire images> <repertoire de sortie> <s, b2, b3> <precision>"
 fi
--- TACHE7/t7_simplif_douglas_bezier.c
+++ TACHE7/t7_simplif_douglas_bezier.c
@@ -1,7 +1,7 @@
 #include "bezier.h"
 
 
-#define SYNTAXE printf("Erreur de syntaxe saisissez: %s <nom fich entree pbm ou contours> <nom fich sortie sans extension> <finesse d>\n", argv[0]);
+// #define SYNTAXE printf("Erreur de syntaxe saisissez: %s <nom fich entree pbm ou contours> <nom fich sortie sans extension> <finesse d>\n", argv[0]);
 
 
 int main(int argc, char** argv) {
